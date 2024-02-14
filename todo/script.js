1ff12a36d7fcff8fb0913d00525dfa3ff3e5fe6e
soo pill <soopill0405@naver.com>
2024-02-14 15:54:51 +0900

    두번째

--- todo/script.js
+++ todo/script.js
@@ -1,14 +1,47 @@
 const todoInput = document.querySelector("#todo-input");
+const todoList = document.querySelector("#todo-list");
+
+const createTodo = function () {
+  const newLi = document.createElement("li");
+  const newSpan = document.createElement("span");
+  const newBtn = document.createElement("button");
+
+  newBtn.addEventListener("click", () => {
+    newLi.classList.toggle("complete");
+  });
+
+  newLi.addEventListener("dblclick", () => {
+    newLi.remove();
+  });
+
+  newSpan.textContent = todoInput.value;
+  newLi.appendChild(newBtn);
+  newLi.appendChild(newSpan);
+  todoList.appendChild(newLi);
+  todoInput.value = "";
+  saveItemsFn();
+};
 
 const keyCodeCheck = function () {
-  if (window.event.keyCode === 13 && todoInput.value !== "") {
-    const todoList = document.querySelector("#todo-list");
-    const newLi = document.createElement("li");
-    const newSpan = document.createElement("span");
+  if (window.event.keyCode === 13 && todoInput.value) {
+    createTodo();
+  }
+};
 
-    newSpan.textContent = todoInput.value;
-    newLi.appendChild(newSpan);
-    todoList.appendChild(newLi);
-    todoInput.value = "";
+const deleteAll = function () {
+  const liList = document.querySelectorAll("li");
+  for (let i = 0; i < liList.length; i++) {
+    liList[i].remove();
+  }
+};
+
+const saveItemsFn = function () {
+  const saveItems = [];
+  for (let i = 0; i < todoList.children.length; i++) {
+    const todoObj = {
+      contents: todoList.children[i].querySelector("span").textContent,
+      complete: todoList.children[i].classList.contains("complete"),
+    };
+    saveItems.push(todoObj);
   }
 };
