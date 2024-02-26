d37bddc9e042eb3eea49628da1f1408382606107
soo pill <soopill0405@naver.com>
2024-02-26 14:09:37 +0900

    2번째

---
+++ codecamp/class/styles/01-02-emotion.js
@@ -0,0 +1,9 @@
+import styled from "@emotion/styled";
+
+export const MyEmail = styled.span`
+  color: blue;
+  fontsize: 25px;
+`;
+export const MyEmailInput = styled.input`
+  border-color: red;
+`;
